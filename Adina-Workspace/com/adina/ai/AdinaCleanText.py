2da341358852d41d8f1f8768dd853a37f2bbbde4
rosamariscal <87385286+rosamariscal@users.noreply.github.com>
2021-07-22 19:20:26 -0500

    Add files via upload
    Biblioteca de limpieza de texto, versión preliminar.

---
+++ Adina-Workspace/com/adina/ai/AdinaCleanText.py
@@ -0,0 +1,33 @@
+#import numpy as np
+#import matplotlib.pyplot as plt
+import json
+
+"""
+This class contains the text cleaning processes to prepare the data for machine learning.
+The routines that make it up are:
+
+"""
+
+
+class CleanOptions:
+
+    def __init__(self):
+        #self.dictionary = self.fromJson(anyJsonString)
+        #print("Nombre documento = ",id(self))
+        print("'Class CleanOptions iniciada'")
+        return None
+
+    def getTextfromDocument(self, var_json):
+
+
+
+        return 0
+    
+    
+
+
+
+
+
+
+
