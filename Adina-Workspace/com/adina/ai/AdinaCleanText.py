28154b13cbbc19b603aaa0381faa49d8ee8433f6
rosamariscal <87385286+rosamariscal@users.noreply.github.com>
2021-07-23 19:58:43 -0500

    Add files via upload
    Manejo de ortografía, caracteres especiales y su prueba

--- Adina-Workspace/com/adina/ai/AdinaCleanText.py
+++ Adina-Workspace/com/adina/ai/AdinaCleanText.py
@@ -1,6 +1,6 @@
-#import numpy as np
-#import matplotlib.pyplot as plt
-import json
+from com.adina.utilities.JSON import JSonObject
+import string
+
 
 """
 This class contains the text cleaning processes to prepare the data for machine learning.
@@ -16,17 +16,47 @@ class CleanOptions:
         #print("Nombre documento = ",id(self))
         print("'Class CleanOptions iniciada'")
         return None
-
-    def getTextfromDocument(self, var_json):
-
-
-
-        return 0
     
+    # Genera una lista con el contenido de las páginas de un registro
+    def getTextfromDocument(self, jsonObject):
+
+        list_paginas = []
+
+        for i in range (len(jsonObject.pages)):
+
+            list_paginas.append(jsonObject.pages[i]['pageContent'])
+
+        return list_paginas
     
+    # Regresa el string a minúsculas
+    def toMinusculas(self, tex_aux):
 
+        return tex_aux.lower()
 
+    # Manejo de ortografía, se estandarizan palabras sin acentos
+    def limpiezaAcentos(self, tex_aux):
 
+        tex_aux = tex_aux.replace("á", "a")
+        tex_aux = tex_aux.replace("é", "e")
+        tex_aux = tex_aux.replace("í", "i")
+        tex_aux = tex_aux.replace("ó", "o")
+        tex_aux = tex_aux.replace("ú", "u")
+
+        return tex_aux
+
+    # Manejo de signos de puntuación y carácteres especiales
+    def limpiezaCaracteresEspeciales(self, tex_aux):
+
+        tex_aux = tex_minus.replace(".", " ")
+        tex_aux = tex_aux.replace(",", " ")
+
+        table = str.maketrans('', '', string.punctuation)
+        tex_aux = tex_aux.translate(table)
+
+        tex_aux = tex_aux.replace("“", "")
+        tex_aux = tex_aux.replace("”", "")
+
+        return tex_aux
 
 
 
