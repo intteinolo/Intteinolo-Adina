c388515239aa80c0afe4ccae2ea021ecfda12143
rosamariscal <87385286+rosamariscal@users.noreply.github.com>
2021-07-28 18:14:09 -0500

    Add files via upload
    Biblioteca de limpieza, e inicio de biblioteca de estadística.

--- Adina-Workspace/com/adina/ai/AdinaCleanText.py
+++ Adina-Workspace/com/adina/ai/AdinaCleanText.py
@@ -4,6 +4,8 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk import sent_tokenize
 from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 
 
 """
@@ -16,8 +18,8 @@ The routines that make it up are:
 class CleanOptions:
 
     def __init__(self):
-        #self.dictionary = self.fromJson(anyJsonString)
-        #print("Nombre documento = ",id(self))
+        
+        self.__configuration = {"configCleanFileName": ""}
         print("'Class CleanOptions iniciada'")
         return None
     
@@ -75,13 +77,6 @@ class CleanOptions:
         # # Descarga de diccionario base
 
         stop_words = stopwords.words('spanish')
-
-        str_alphabet = string.ascii_lowercase
-        list_alphabet = list(str_alphabet)
-
-        for i in range(len(list_alphabet)):
-            
-            stop_words.append(list_alphabet[i])
 
         bad_words = []
         for i in range(len(stop_words)):
@@ -157,19 +152,6 @@ class CleanOptions:
         bad_words.append("incluso")
         #bad_words.append("condicion")
 
-        # # letras y números aislados
-
-        bad_words.append("0")
-        bad_words.append("1")
-        bad_words.append("2")
-        bad_words.append("3")
-        bad_words.append("4")
-        bad_words.append("5")
-        bad_words.append("6")
-        bad_words.append("7")
-        bad_words.append("8")
-        bad_words.append("9")
-
         return bad_words
 
     # # Eliminación de preposiciones del español
@@ -202,34 +184,131 @@ class CleanOptions:
                 list_new.append(word)
         
         return list_new
+    
+    # # # Identifica idioma, descarta palabras que no estén en español
+    # def identificaIdioma(self, list_aux):
 
+    #     import cld3
+    
+    #     list_new = []
+    #     for word in list_aux:
 
+    #         cld3.get_language("影響包含對氣候的變化以及自然資源的枯竭程度")
+    #         if TextBlob(word).detect_language() == "es":
 
+    #                 list_new.append(word)
 
+    #     return list_new
+    
+    # # Diccionario de abreviaturas
+    def diccionarioAbreviaturas(self):
+        
+        bad_words = []
+        str_alphabet = string.ascii_lowercase
+        list_alphabet = list(str_alphabet)
 
+        for i in range(len(list_alphabet)):
+            
+            bad_words.append(list_alphabet[i])
+        
+        # # números aislados
 
+        bad_words.append("0")
+        bad_words.append("1")
+        bad_words.append("2")
+        bad_words.append("3")
+        bad_words.append("4")
+        bad_words.append("5")
+        bad_words.append("6")
+        bad_words.append("7")
+        bad_words.append("8")
+        bad_words.append("9")
 
+        two_letters = []
+        for i in range(len(bad_words)):
 
+            for j in range(len(bad_words)):
 
+                aux = ''.join([bad_words[i], bad_words[j]])
+                two_letters.append(aux)
 
+        three_letters = []
+        for i in range(len(two_letters)):
 
+            for j in range(len(bad_words)):
 
+                aux = ''.join([two_letters[i], bad_words[j]])
+                three_letters.append(aux)
+        
+        for i in range(len(two_letters)):
 
+            bad_words.append(two_letters[i])
+        
+        for i in range(len(three_letters)):
 
+            bad_words.append(three_letters[i])
 
+        return bad_words
 
+    # # Eliminación de abreviaturas español
+    # # En este caso las abreviaturas se eliminan pero otra opción sería ampliar las palabras, ejemplo: sr:señor
+    def eliminacionAbreviaturas(self, list_aux):
 
+        # # Traer diccionario de abreviaturas
+        bad_words = self.diccionarioAbreviaturas()
+        
+        list_aux = [w for w in list_aux if not w in bad_words]
 
+        return list_aux
 
+    # # Lemmatización de las palabras
+    # # Es reducir las formas flexivas a una forma básica común. Utiliza bases de conocimiento léxico para obtener las formas básicas correctas de las palabras.
+    def lematizacionPalabras(self, list_aux):
 
+        lemmatizer = WordNetLemmatizer()
+        for i in range(len(list_aux)):
 
+            list_aux[i] = lemmatizer.lemmatize(list_aux[i])
+        
+        return list_aux
 
+    # # La lista se depura a valores únicos
+    def valoresUnicos(self, list_aux):
 
+        list_new = []
+        for i in range(len(list_aux)):
+            
+            if list_aux[i] not in list_new:
+                
+                list_new.append(list_aux[i])
 
+        return list_new
 
+    # # Rutina de limpieza completa
+    def rutinaLimpieza(self, tex_aux):
 
+        # # Conversión a minúsculas
+        tex_aux = self.toMinusculas(tex_aux)
 
+        # # Tratamiento ortográfico
+        tex_aux = self.limpiezaAcentos(tex_aux)
 
+        # # Manejo de signos de puntuación y carácteres especiales
+        tex_aux = self.limpiezaCaracteresEspeciales(tex_aux)
+
+        # # Separación por palabras
+        list_aux = self.separarPalabras(tex_aux)
+
+        # # Eliminación de preposiciones del español
+        list_aux = self.eliminacionPreposiciones(list_aux)
+
+        # # Eliminación de abreviaturas
+        list_aux = self.eliminacionAbreviaturas(list_aux)
+
+        # # Lemmatización de las palabras
+        list_aux = self.lematizacionPalabras(list_aux)
+
+        return list_aux
 
 
 
---
+++ Adina-Workspace/com/adina/ai/AdinaStatisticText.py
@@ -0,0 +1,38 @@
+from AdinaCleanText import CleanOptions
+
+class StatisticText:
+
+    def __init__(self):
+        
+        self.__configuration = {"configStatisticFileName": ""}
+        print("'Class StatisticText iniciada'")
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
